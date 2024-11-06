7c480c9074aa7f8b5b198d85580d3a71edd318e3
Thulasirajan1103 <thulasithulasi1103@gmail.com>
2024-11-06 20:35:21 +0530

    add reports1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+import axios from 'axios';
+
 
 const studentRoutes = require('./routes/studentRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
